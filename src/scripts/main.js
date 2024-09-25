da26e1c583c10fef72fbd8cfa4efe90010735be0
Aria <arianaderi240@gmail.com>
2024-09-25 13:46:49 +0330

    successful part is done / bugs that were related to check state fixed !

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -52,6 +52,8 @@ const wordsList = [
   "YUMMY",
   "ZESTY",
 ];
+let alertBox = document.querySelector("#alert-box");
+let alertMessage = document.querySelector("#alert-message");
 let rowStep = 0;
 let cellStep = 0;
 let rows = document.querySelectorAll(".row_div");
@@ -62,16 +64,20 @@ let btnPress;
 //*creating random numbers for selecting different word from word list each time
 const selectedWordIndex = Math.floor(Math.random() * 50);
 let selectedWord = wordsList[selectedWordIndex];
+console.log(selectedWord);
 
 //? Functions
 const deleteChar = () => {
   //* check if there is letter in cells
-  if (cellStep >= 0) {
-    rowCells[cellStep].style.backgroundColor = "";
+  if (cellStep > 0) {
+    console.log(cellStep);
+
     //*check if the selected cell isn't full
     if (rowCells[cellStep].innerHTML !== "") {
       rowCells[cellStep].innerHTML = "";
+      rowCells[cellStep].style.backgroundColor = "";
     }
+
     //* check if the selected cell is empty , jump one cell back and then make them empty
     else if (rowCells[cellStep].innerHTML === "") {
       cellStep--;
@@ -100,12 +106,21 @@ const loopOnRows = (e) => {
       rowCells = rows[rowStep].children;
       if (cellStep < 5) {
         for (let i = 0; i < selectedWord.length; i++) {
-          //* change cell color to yellow if the lette is true but It isn't at the right index
-          if (selectedWord[i] === btnPress) {
+          //* change cell color to green if the letter exists in the word and it is at the right place
+          if (cellStep === i && selectedWord[i] === btnPress) {
+            rowCells[cellStep].style.backgroundColor = "green";
+            //* if all cells are green , it means that user found the word , show the successful alert !
+            if (
+              Object.values(rowCells).every(
+                (row) => row.style.backgroundColor === "green"
+              )
+            ) {
+              alertMessage.textContent = `Successfull "${selectedWord}"`;
+              alertBox.style.transform = "translateY(25px)";
+              document.removeEventListener("keydown", loopOnRows);
+            } //*  change cell color to yellow if the letter exists in the word but it isn't at the right place
+          } else if (cellStep !== i && selectedWord[i] === btnPress) {
             rowCells[cellStep].style.backgroundColor = "#FFEE8C";
-            //* change cell color to green if the lette is true and It's at the right index
-            if (i === cellStep)
-              rowCells[cellStep].style.backgroundColor = "green";
           }
         }
         //*check if It's in cell length
