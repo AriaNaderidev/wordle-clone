2c842d294554e5130cf18d268caed9d07fb987ab
Aria <arianaderi240@gmail.com>
2024-09-29 16:41:09 +0330

    bug fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -59,25 +59,20 @@ let cellStep = 0;
 let rows = document.querySelectorAll(".row_div");
 let rowCells;
 let alphabetRegexPatern = /^[A-Z]+$/;
-let letterKeyPress;
-const selectedWordIndex = Math.floor(Math.random() * 50);
+let letterKeyPress = "";
+let screenKeyBoardBtns = document.querySelectorAll("button[name]");
+const selectedWordIndex = Math.floor(Math.random() * wordsList.length);
 let selectedWord = wordsList[selectedWordIndex];
 console.log(selectedWord);
 
 //? Functions
 const deleteCharactersFromRowCells = () => {
-  // debugger;
   if (cellStep === 0) return;
   cellStep--;
   if (rowCells[cellStep].innerHTML !== "") {
-    // debugger;
-
     rowCells[cellStep].innerHTML = "";
     rowCells[cellStep].style.backgroundColor = "";
   } else if (rowCells[cellStep - 1].innerHTML === "") {
-    // debugger;
-
-    // debugger;
     cellStep--;
     rowCells[cellStep].innerHTML = "";
     rowCells[cellStep].style.backgroundColor = "";
@@ -94,62 +89,89 @@ const allCellsAreGreen = (rowCells) => {
 };
 
 const loopOnRowsAndPrintLettersAtCells = (letter) => {
-  letterKeyPress = letter.key.toUpperCase();
+  console.log(letter);
+
   if (rowStep > 5) return;
+
   rowCells = rows[rowStep].children;
 
-  if (cellStep === 5 && letter.key === "Enter") {
-    if (rowStep > 5) return;
-    console.log(cellStep, "enter");
+  if (alphabetRegexPatern.test(letter) && letter.length === 1) {
+    console.log("Entered here 1 -- first condition");
 
-    ++rowStep;
-    cellStep = 0;
-    return;
-  }
-  if (cellStep - 1 < rowCells.length && letter.key === "Backspace") {
-    deleteCharactersFromRowCells();
-    return;
-  }
+    letterKeyPress = letter;
 
-  if (alphabetRegexPatern.test(letterKeyPress) && letterKeyPress.length === 1)
-    if (rowStep < rows.length) {
+    if (cellStep < rowCells.length) {
+      rowCells[cellStep].innerHTML = letterKeyPress;
+
       if (selectedWord[cellStep] === letterKeyPress) {
         rowCells[cellStep].style.backgroundColor = "green";
       } else if (selectedWord.includes(letterKeyPress)) {
-        if (cellStep > 4) return;
         rowCells[cellStep].style.backgroundColor = "#FFEE8C";
       }
 
-      if (allCellsAreGreen(rowCells)) {
-        alertMessage.textContent = `Successfull "${selectedWord}"`;
-        alertBox.style.transform = "translateY(25px)";
-        document.removeEventListener(
-          "keydown",
-          loopOnRowsAndPrintLettersAtCells
-        );
-      } else if (rowStep === 5 && cellStep >= 4) {
-        alertMessage.textContent = `Lose`;
-        alertBox.style.transform = "translateY(25px)";
-        document.removeEventListener(
-          "keydown",
-          loopOnRowsAndPrintLettersAtCells
-        );
-      }
-
-      if (cellStep > 4) return;
-
-      if (cellStep < rowCells.length) {
-        cellStep++;
-        rowCells[cellStep - 1].innerHTML = letterKeyPress;
-      }
+      cellStep++;
+      letterKeyPress = "";
     }
+
+    if (allCellsAreGreen(rowCells)) {
+      alertMessage.textContent = `Successfull "${selectedWord}"`;
+      alertBox.style.transform = "translateY(25px)";
+      document.removeEventListener("keydown", loopOnRowsAndPrintLettersAtCells);
+    } else if (rowStep === 5 && cellStep > 4) {
+      alertMessage.textContent = `Lose`;
+      alertBox.style.transform = "translateY(25px)";
+      document.removeEventListener("keydown", loopOnRowsAndPrintLettersAtCells);
+    }
+  } else if (letter === "ENTER") {
+    console.log("PRESSED ENTER");
+
+    letterKeyPress = "";
+    console.log(letterKeyPress);
+
+    if (cellStep === 5) {
+      rowStep++;
+      cellStep = 0;
+      letterKeyPress = "";
+    }
+    return;
+  } else if (letter === "BACKSPACE") {
+    deleteCharactersFromRowCells();
+  }
+  letter = "";
 };
 
-// const animationsForEnterAndBackspaceBtn = (letter) => {
-//   const enter = [letter.key === "Enter"] ?? {};
-//   console.log(enter);
-// };
-
 //? Add event listener
-document.addEventListener("keydown", loopOnRowsAndPrintLettersAtCells);
-// document.addEventListener("keydown", animationsForEnterAndBackspaceBtn);
+document.addEventListener("keydown", (event) =>
+  loopOnRowsAndPrintLettersAtCells(event.key.toUpperCase())
+);
+
+// screenKeyBoardBtns.forEach((btn) => {
+//   btn.addEventListener("click", (event) => {
+//     let keyValue = event.target.name.toUpperCase();
+
+//     // if (alphabetRegexPatern.test(keyValue)) {
+//     //   if (cellStep > 5) return;
+//     //   loopOnRowsAndPrintLettersAtCells(keyValue);
+//     //   letterKeyPress = "";
+//     // }
+
+//     let keyPressEvent = new KeyboardEvent("keypress", {
+//       key: keyValue,
+//       code: `Key${keyValue}`,
+//       keyCode: keyValue.charCodeAt(0),
+//       bubbles: true,
+//     });
+
+//     keyValue = "";
+
+//     document.dispatchEvent(keyPressEvent);
+//     document.removeEventListener("keypress", keyPressEvent);
+//   });
+// });
+
+screenKeyBoardBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    let keyValue = event.target.name.toUpperCase();
+    loopOnRowsAndPrintLettersAtCells(keyValue);
+  });
+});
